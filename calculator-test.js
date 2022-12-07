5cd5b6dddf87357cb710ab2306649f014cff472c
Sean <seanmcdermott701@gmail.com>
2022-12-07 00:53:23 -0700

    add all calc files

---
+++ calculator-test.js
@@ -0,0 +1,16 @@
+
+it('should calculate the monthly rate correctly', function () {
+  expect(calculateMonthlyPayment({amount: 10000, years: 10, rate: 4.5})).toEqual('103.64');
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  let vals = {
+    amount: 20000,
+    years: 15,
+    rate: 50
+  }
+  expect(calculateMonthlyPayment(vals)).toEqual('833.87')
+});
+
+/// etc
